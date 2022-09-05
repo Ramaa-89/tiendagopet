b3b03cf90a33aebedb2e3554bbb69b4a9f4149db
Ramiro Morales <r_morales_89@outlook.com>
2022-09-04 22:15:23 -0300

    commit on main

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import NavBar from './components/navbar/NavBar';
 import ClassComponent from './components/ClassComponent/ClassComponent';
 import FunctionalComponent from './components/FunctionalComponent/FunctionalComponent';
 import Boton from './components/boton/Boton';
+import Counter from './components/counter/counter';
 
 
 function App() {
+  const stock = 10
+
   return (
     <div className="App">
         <NavBar/> 
@@ -13,6 +16,8 @@ function App() {
         <FunctionalComponent/>
         <ClassComponent/>
         <Boton/>
+
+        <Counter  stock={stock} />
       </header>
     </div>
   );
--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
+import Swal from 'sweetalert';
+import "./counter.css"
 
-const Counter = () => {
-    const [stock, setStock] = useState(10)
+const Counter = (props) => {
+    
+    const [cantidad, setCantidad] = useState(0)
+
+    const sumar = () => cantidad <= props.stock -1 ? setCantidad (cantidad + 1) : Swal("No hay más disponible!", {
+        button: {
+            text: "Volver",
+          },
+      });
+
+    const restar = () => cantidad > 0 ? setCantidad (cantidad - 1) : Swal("No puede ser negativo", {timer: 1000, buttons:false,});
 
     return (
         <>
-        <div> {stock} </div>
+        <div className="descripcion">Seleccionaste {cantidad} monedas</div>
+        <div className="contador">
+            <button className="btn-counter btn btn-primary" onClick={restar} placeholder="-"> - </button>
+            <button className="btn-counter btn btn-primary" onClick={sumar} placeholder="+"> + </button>
+        </div>
         </>
     )
 
